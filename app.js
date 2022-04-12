a1d0aa5ec3c2ba29074c64c9a5ce517cc305897c
patelbimal5 <68713222+patelbimal5@users.noreply.github.com>
2022-04-11 22:23:23 -0400

    complete running blog site.
    adding post and postnotfound ejs to check and run individual post pages and stories.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require(lodash);
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -38,9 +38,6 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  //let postTitle = req.body.postTitle;
-  //let postBody = req.body.postContent;
-
   const post = {
     title : req.body.postTitle,
     content: req.body.postContent
@@ -51,14 +48,16 @@ app.post("/compose", function(req, res){
 
 
 app.get("/post/:postName", function(req, res){
-  const postNameCheck = req.params.postName;
+  const postNameCheck = _.lowerCase(req.params.postName);
 
   posts.forEach(function(post){
-    const storedTitle = post.title;
+    const storedTitle = _.lowerCase(post.title) ;
     if(storedTitle === postNameCheck){
-      console.log("Match found")
+      res.render("post", {
+        titlePost : post.title,
+        contentPost : post.content});
     }else{
-      console.log("Naa not matched.");
+      res.render("postnotfound", {titlePost : postNameCheck})
     }
   });
 
